62a258f190293a849c1ab3a0a43f6ee25a4a071c
alexblls <alextortosa2001@gmail.com>
2022-03-26 18:01:39 +0100

    juntacion de scripts(atenderCliente y cambio de camara)
    cambio de camara y atender cliente se han juntado en un script para controlar el funcionamiento

--- Kebab Simulator/Assets/_MiProyecto/Personajes/Scripts/atenderCliente.cs
+++ Kebab Simulator/Assets/_MiProyecto/Personajes/Scripts/atenderCliente.cs
@@ -7,11 +7,15 @@ public class atenderCliente : MonoBehaviour
 {
     public Transform destino;
     public NavMeshAgent jugador;
+    public GameObject cliente;
     public GameObject vista;
+    public Camera camaraPrincipal;
+    public Camera camaraSecundaria;
 
     private void Start()
     {
         vista.SetActive(false);
+        camaraSecundaria.gameObject.SetActive(false);
     }
 
     private void LateUpdate()
@@ -19,6 +23,25 @@ public class atenderCliente : MonoBehaviour
         if (jugador.remainingDistance >= 0.001 && jugador.remainingDistance <= 0.2)
         {
             vista.SetActive(true);
+
+            Ray ray = camaraPrincipal.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(cliente.name + " " + hit.transform.name);
+
+                //Select stage    
+                if (hit.transform.name == cliente.name)
+                {
+                    camaraSecundaria.gameObject.SetActive(true);
+                    camaraPrincipal.gameObject.SetActive(false);
+                }
+                else
+                {
+                    camaraSecundaria.gameObject.SetActive(false);
+                    camaraPrincipal.gameObject.SetActive(true);
+                }
+            }
         }
     }
 
