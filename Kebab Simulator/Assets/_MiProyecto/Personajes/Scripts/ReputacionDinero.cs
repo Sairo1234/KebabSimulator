7baef4e2c93e9e20f858a35ad4729b3e881878b4
AdriTuR <74455555+AdriTuR@users.noreply.github.com>
2022-04-21 20:03:18 +0200

    Añadido sistema de Reputación y Dinero

---
+++ Kebab Simulator/Assets/_MiProyecto/Personajes/Scripts/ReputacionDinero.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ReputacionDinero : MonoBehaviour
+{
+
+    //-------------------------------------------------------------------------------------//
+    //----------------------------------- REPUTACIÓN --------------------------------------//
+
+    int maxReputacion;
+    public float Reputacion = 0;
+    public Image BarraReputacion;
+    public Text texto_Nivel;
+    int Nivel = 1;
+
+    //----------------------------------------------------------------------------------//
+    //----------------------------------- DINERO --------------------------------------//
+
+    public Text texto_Dinero;
+    public float Dinero = 0;
+
+    //----Constante----//
+    float d = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxReputacion = 100;
+        texto_Nivel.text = "0";
+        texto_Dinero.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MostrarDinero();
+        MostrarReputacion();
+    }
+
+    //-------------------------------------------------------------------------------------//
+    //----------------------------------- REPUTACIÓN --------------------------------------//
+
+    public void TakeReputacion(float reputacion)
+    {
+
+        Reputacion += reputacion;
+        Reputacion = Mathf.Clamp(Reputacion, 0, maxReputacion);
+
+        if (Reputacion >= maxReputacion)
+        {
+            Nivel++;
+            Reputacion = 1;
+        }
+        else if(Reputacion == 0 && Nivel !=0 )
+        {
+            Nivel--;
+            Reputacion = 99;
+        }
+        else if(Reputacion == 0 && Nivel == 0)
+        {
+            Debug.Log("Has perdido");
+        }
+    }
+
+    void MostrarReputacion()
+    {
+        BarraReputacion.fillAmount = Reputacion / maxReputacion;
+        texto_Nivel.text = Nivel.ToString();
+    }
+
+    //----------------------------------------------------------------------------------//
+    //----------------------------------- DINERO --------------------------------------//
+
+    public void TakeDinero(float dinero)
+    {
+        Dinero += dinero;
+    }
+
+    void MostrarDinero()
+    {
+        texto_Dinero.text = Dinero.ToString();
+    }
+
+}
--- Kebab Simulator/Assets/_MiProyecto/Personajes/Scripts/SalidaCliente.cs
+++ Kebab Simulator/Assets/_MiProyecto/Personajes/Scripts/SalidaCliente.cs
@@ -10,7 +10,7 @@ public class SalidaCliente : MonoBehaviour
     private bool estaDesplazandose = false;
 
     NavMeshAgent cliente;
-
+    private GameObject jugador;
 
 
     void Start()
@@ -18,14 +18,21 @@ public class SalidaCliente : MonoBehaviour
         cliente = GetComponent<NavMeshAgent>();
         GameObject GameObjectsalida = GameObject.FindGameObjectWithTag("Salida");
         TransformSalida = GameObjectsalida.transform;
+
+        jugador = GameObject.FindGameObjectWithTag("Player");
     }
 
     public void salir()
     {
+        float penalizacionRechazar = -25;
+        jugador.GetComponent<ReputacionDinero>().TakeReputacion(penalizacionRechazar);
+
         this.transform.parent.gameObject.transform.DetachChildren();
         this.gameObject.GetComponent<EntradaCliente>().enabled = false;
         cliente.SetDestination(TransformSalida.position);
         estaDesplazandose = true;
+
+        
     }
 
     
