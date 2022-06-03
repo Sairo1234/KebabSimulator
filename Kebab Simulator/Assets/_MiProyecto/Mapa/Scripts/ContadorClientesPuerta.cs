7333fcd9c9fa783d7e44738c672e6dc6ca4d1b74
ml026 <maria.lahuerta.m@gmail.com>
2022-06-03 19:58:31 +0200

    Sonido Puerta

--- Kebab Simulator/Assets/_MiProyecto/Mapa/Scripts/ContadorClientesPuerta.cs
+++ Kebab Simulator/Assets/_MiProyecto/Mapa/Scripts/ContadorClientesPuerta.cs
@@ -6,15 +6,19 @@ public class ContadorClientesPuerta : MonoBehaviour
 {
     public int contadorClientes;
     public int contadorClientesSpawn;
+    public AudioSource audioSourceEntrada;
+    public AudioSource audioSourceSalida;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "ClienteSaliendo")
         {
             contadorClientes++;
+            audioSourceSalida.Play();
         }
         if (other.gameObject.tag == "Cliente")
         {
             contadorClientesSpawn++;
+            audioSourceEntrada.Play();
         }
 
     }
